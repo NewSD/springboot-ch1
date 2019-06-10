d82d6a8ef0f1ac89bdb51a7a2a976b9a112b4fbc
18300603955 <oceaniu@163.com>
2019-06-10 16:58:24 +0800

    prod

--- src/main/java/com/niu/springboot/SpringbootCh1Application.java
+++ src/main/java/com/niu/springboot/SpringbootCh1Application.java
@@ -15,7 +15,9 @@ public class SpringbootCh1Application {
     public static void main(String[] args) {
 //        关闭banner
         SpringApplicationBuilder builder = new SpringApplicationBuilder(SpringbootCh1Application.class);
+        builder.application().setAdditionalProfiles("prod");
         builder.bannerMode(Banner.Mode.OFF).run(args);
+
 //        SpringApplication.run(SpringbootCh1Application.class, args);
     }
 
